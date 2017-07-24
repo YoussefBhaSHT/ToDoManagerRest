22d68b659bb41987d3e2db6ba80c7390e8750b3f
BELHADJ AISSA <youssef.belhadj-aissa@sogeti.com>
2017-07-24 16:04:00 +0200

    Ajoutez des fichiers projet.

---
+++ ToDoManagerRest/ToDoManagerRest/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagerRest.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoManagerRest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            User u;
+            
+        }
+    }
+}
---
+++ ToDoManagerRest/ToDoManagerRest/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoManagerRest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password{ get; set; }
+        public string Api_Key { get; set; }
+        public int Status { get; set; }
+        
+    }
+}
